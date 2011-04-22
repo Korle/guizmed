2a48516047339606ccf264d47e40f38387bee436
kBoussy <the_chosen_dragon@hotmail.com>
2011-04-22 11:55:53 +0200

    Guizmed- test communicatie met frontend

--- GuizMed/apps/backend/modules/patienten/templates/indexSuccess.php
+++ GuizMed/apps/backend/modules/patienten/templates/indexSuccess.php
@@ -1,28 +1,16 @@
-<h1>Ad patients List</h1>
-
-<table>
-  <thead>
-    <tr>
-      <th>Patient</th>
-      <th>Fname</th>
-      <th>Lname</th>
-      <th>Bdate</th>
-      <th>Patient since</th>
-      <th>Sex</th>
-    </tr>
-  </thead>
-  <tbody>
+{
+    "allPatients" : [
+    <?php $bol = true?>
     <?php foreach ($ad_patients as $ad_patient): ?>
-    <tr>
-      <td><a href="<?php echo url_for('patienten/show?patient_id='.$ad_patient->getPatientId()) ?>"><?php echo $ad_patient->getPatientId() ?></a></td>
-      <td><?php echo $ad_patient->getFname() ?></td>
-      <td><?php echo $ad_patient->getLname() ?></td>
-      <td><?php echo $ad_patient->getBdate() ?></td>
-      <td><?php echo $ad_patient->getPatientSince() ?></td>
-      <td><?php echo $ad_patient->getSex() ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+    <?php if($bol==true){
+        echo ",";
+    }?>
+    <?php $bol=false ?>
+    {
+            "id" : "<?php echo $ad_patient->getPatientId() ?>",
+            "name": "<?php echo $ad_patient->getFname() ?>"
+        }
 
-  <a href="<?php echo url_for('patienten/new') ?>">New</a>
+    <?php endforeach; ?>
+    ]
+}
